8ea2c547b3c909d93d3236ef1a78c7f4db640211
dangthaison98 <dangthaison98job@gmail.com>
2024-11-18 11:33:32 +0700

    Add script

---
+++ Assets/Minigame_Wood_Worm/Scripts/LevelHolder.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DTS.Woodworm
+{
+    public class LevelHolder : MonoBehaviour
+    {
+        
+    }
+}
